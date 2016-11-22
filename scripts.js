95bded5df6344f5cf0e6d99ccd911893bbe55ae6
Daniel Shin <shin.daniel.d@gmail.com>
2016-11-22 14:36:42 -0700

    add guess, clear, reset functionality

--- scripts.js
+++ scripts.js
@@ -2,16 +2,56 @@ var randomNumber = generateNumber();
 var userInput = document.getElementById('user-input');
 var guess = document.getElementById('guess');
 var clear = document.getElementById('clear');
+var reset = document.getElementById('reset');
 var resultsText = document.getElementById('results-text');
 
-function generateNumber() {
-  return Math.floor((Math.random() * 100) + 1);
-}
 
 guess.addEventListener('click', function() {
   checkGuess();
 });
 
+clear.addEventListener('click', function() {
+  clearInput();
+});
+
+reset.addEventListener('click', function() {
+  resetAll();
+});
+
+function generateNumber() {
+  return Math.floor((Math.random() * 100) + 1);
+}
+
+function clearInput() {
+  userInput.value = '';
+}
+
+function resetAll() {
+  clearInput();
+  randomNumber = generateNumber();
+  return resultsText.innerText = 'Guess a number!';
+}
+
 function checkGuess() {
-  return resultsText.innerText = 'Correct!';
+  var inputValue = parseInt(userInput.value);
+
+  if(inputValue > 100 || inputValue < 0) {
+    return resultsText.innerText = 'Select number between 0 and 100!';
+  }
+  if (inputValue < randomNumber - 5) {
+    return resultsText.innerText = 'Way too low!';
+  }
+  if (inputValue > randomNumber + 5) {
+    return resultsText.innerText = 'Way too high!';
+  }
+  if (inputValue < randomNumber) {
+    return resultsText.innerText = 'Too low!';
+  }
+  if (inputValue > randomNumber) {
+    return resultsText.innerText = 'Too high!';
+  }
+  if (inputValue == randomNumber) {
+    return resultsText.innerText = 'Correct!';
+  }
+
 }
